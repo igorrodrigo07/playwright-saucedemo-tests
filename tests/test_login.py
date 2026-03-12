ef77fbc27ca5e269dd18f5d9d7120d14e5b63d2b
Igor Rodrigo dos Santos <igor.rodriigo@yahoo.com>
2026-03-12 01:41:23 -0300

    First automation test with Playwright

---
+++ tests/test_login.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import sync_playwright
+
+def test_login():
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://www.saucedemo.com")
+
+        page.fill("#user-name", "standard_user")
+        page.fill("#password", "secret_sauce")
+        page.click("#login-button")
+
+        page.wait_for_selector(".inventory_list")
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    test_login()
